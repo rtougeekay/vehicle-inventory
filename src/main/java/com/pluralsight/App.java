c7081ac8366a645563a69fdf5727c0e4db1853fc
J Richard K Tougeekay <rtougeekay@gmail.com>
2026-04-16 16:03:41 -0500

    Vehicle exercise

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class App {
+private static Scanner Input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+
+        Vehicle[] vehicles = new Vehicle[6];
+        vehicles[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
+        vehicles[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
+        vehicles[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
+        vehicles[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
+        vehicles[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
+        vehicles[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
+
+
+
+
+    int count = 0;
+ while(count< 6) {
+     System.out.println(vehicles[count].getColor());
+     System.out.println(vehicles[count].getMakeModel());
+     System.out.println(vehicles[count].getPrice());
+     System.out.println(vehicles[count].getOdometerReading());
+     System.out.println(vehicles[count].getVehicleId());
+     count++;
+
+    }
+
+
+}
 }
